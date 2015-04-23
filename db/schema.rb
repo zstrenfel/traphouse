25c757b57210f6ef7a2bbb1607ebc369b399a54e
josh <howland_josh@yahoo.com>
2015-04-22 19:09:42 -0700

    Aded coke, water, baking soda and money field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423020223) do
+ActiveRecord::Schema.define(version: 20150423020906) do
 
   create_table "houses", force: :cascade do |t|
     t.string   "name"
@@ -44,6 +44,10 @@ ActiveRecord::Schema.define(version: 20150423020223) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "coke"
+    t.integer  "water"
+    t.integer  "baking_soda"
+    t.integer  "money"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
