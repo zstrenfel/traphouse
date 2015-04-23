40af52f9585fb6dca73a19823f2b10b1f3ebdb2e
Zach Strenfel <zwstrenfel@gmail.com>
2015-04-22 19:04:08 -0700

    added houses controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423015940) do
+ActiveRecord::Schema.define(version: 20150423020223) do
+
+  create_table "houses", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.text     "question"
