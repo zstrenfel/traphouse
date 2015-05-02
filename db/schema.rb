149646d5933155e8dc0e0b7f708e2a0fd82645a2
josh <howland_josh@yahoo.com>
2015-05-02 11:46:07 -0700

    Added profile pic column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423022809) do
+ActiveRecord::Schema.define(version: 20150502184516) do
 
   create_table "houses", force: :cascade do |t|
     t.string   "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150423022809) do
     t.integer  "baking_soda"
     t.integer  "money"
     t.string   "name"
+    t.string   "profile_picture_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
